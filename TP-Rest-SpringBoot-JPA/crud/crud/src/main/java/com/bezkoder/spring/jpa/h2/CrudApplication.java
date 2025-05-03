4942d81abfd7e64268934931c66f72e166afe464
Karen Khoury <129860242+KarenKhou@users.noreply.github.com>
2025-05-03 18:16:57 +0300

    REST API + SPRING BOOT + JPA

---
+++ TP-Rest-SpringBoot-JPA/crud/crud/src/main/java/com/bezkoder/spring/jpa/h2/CrudApplication.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.jpa.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
